9dbd6728a913fbeee067af2f0e3102647f797114
BugBuster99 <arvindj19962014@gmail.com>
2016-12-28 20:52:17 +0530

    Update shift.c

--- shift.c
+++ shift.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 void main() {
 	int rot,n,rem,shif,iter;
+	iter=1;
 	scanf("%d%d",&n,&rot);
 	rot=rot%5;
 	iter=5-rot;
-	rem=n/(10*iter);
-	shif=n%(10*iter);
+	while(iter<m)
+	{
+		iter=iter*10;
+	}
+	rem=n/(iter);
+	shif=n%(iter);
 	shif=shif*(10*rot)+rem;
 	printf("The shifted number is: %d",shif);
 	
