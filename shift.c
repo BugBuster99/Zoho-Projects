5dd3be53db02ad98ed74ad6a2b5b7e64d8cc1fe6
BugBuster99 <arvindj19962014@gmail.com>
2016-12-28 18:13:31 +0530

    Create new file

---
+++ shift.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main() {
+	int rot,n,rem,shif;
+	scanf("%d %d",&n,&rot);
+	while(rot>0)
+	{
+	    rem=n%10000;
+	    shif=(rem*10)+(n/10000);
+	    rot--;
+	}
+	printf("The shifted number is: %d",shif);
+	
+}
+
