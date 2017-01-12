e22e7e55dfeeadaca2a0344777a3fa72fb54c741
BugBuster99 <arvindj19962014@gmail.com>
2017-01-12 21:11:06 +0530

    Update Intpalingenerate.c

--- Intpalingenerate.c
+++ Intpalingenerate.c
@@ -1,22 +1,44 @@
-#include<stdio.h>
-void main(){
-    int n,rev,i,a,rem,count;
-    scanf("%d",&n);
-    rev=0;
-    count=0;
-    a=n/10;
-    while(a>0){
-        rem=a%10;
-        rev=rev*10+rem;
-        count++;
-        a=a/10;
-    }
-    i=1;
-    while(count>0){
-        i=i*10;
-        count--;
-    }
-    rev=n*i+rev;
-    printf("%d",rev);
-    
+#include <stdio.h>
+
+int main(void) {
+	int n,digits,i,j,temp;
+	scanf("%d",&n);
+	digits=0;
+	temp=n;
+	while(temp>0){
+	    digits+=1;
+	    temp=temp/10;
+	}
+
+	temp=n;
+	
+	int arr[digits];
+	for(i=(digits-1);i>=0;i--){
+	    arr[i]=(temp%10);
+	    temp=temp/10;
+	    
+	}
+	j=digits-1;
+	i=0;
+	while(i<=j){
+	    
+	    if(arr[i]!=arr[j]){
+	        if(arr[i]>arr[j]){
+	            arr[j]=arr[i];
+	        }
+	        else{
+	            arr[j]=arr[i];
+	            arr[j-1]+=1;
+	        }
+	    }
+	    if(arr[i]==arr[j]){
+	        i++;
+	        j--;
+	    }
+	}
+	for(i=0;i<digits;i++){
+	    printf("%d",arr[i]);
+	}
+	return 0;
 }
+
