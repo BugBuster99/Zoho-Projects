d0610a0584190aa0f52bc8d8c78228ee636df744
BugBuster99 <arvindj19962014@gmail.com>
2017-01-12 17:41:29 +0530

    Create nextprime.c

---
+++ nextprime.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,n,flag,temp;
+	flag=0;
+	scanf("%d",&n);
+	temp=n+1;
+	while(flag==0){
+	    for(i=2;i<(n/2);i++){
+	        if((temp%i)==0){
+	            flag=0;
+	            
+	            break;
+	        }
+	        else{
+	            flag=1;
+	        }
+	    }
+	    if(flag==1){
+	        printf("%d",temp);
+	    }
+	    else{
+	        temp++;
+	    }
+	}
+	return 0;
+}
+
