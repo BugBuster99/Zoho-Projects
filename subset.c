be54d9209eee161a00a4aa392ab380876e1cdefc
BugBuster99 <arvindj19962014@gmail.com>
2017-01-24 22:03:34 +0530

    Update subset.c

--- subset.c
+++ subset.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-
+#include<math.h>
+void compute(int *s, int size)
+{
+    int total;
+    total=pow(2,size);                      //the limit of the binary counter is computed here
+    int bcount,i;
+    for(bcount=0;bcount<total;bcount++)
+    {
+        for(i=0;i<size;i++)                     
+        {
+            if((1<<i) & bcount )        // the counter value is compared bitwise with 1 left shifted 'i' times
+            {
+                printf("%d,",s[i]);     // the bits in the array are printed corresponding to the bits of the counter 
+            }
+        }
+        printf("\n");
+    }
+}
 int main(void) {
-	int n,i,j,k;
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
+	int s[100];                                //input set is obtained as character array
+	int i,size;
+	scanf("%d",&size);                             //size of the array is obtained
+	for(i=0;i<size;i++)
 	{
-	    scanf("%d",&a[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-	    for(j=i;j<n;j++)
-	    {
-	        for(k=i;k<=j;k++)
-	        {
-	            printf("%d,",a[k]);
-	        }
-	        printf("\n");
-	    }
+	    scanf("%d",&s[i]);                     
 	}
 	
+	compute(s,size);                            //the array and it's size are passed as arguments to the function
 	return 0;
 }
 
