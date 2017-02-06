91f20a7507dabc90f023a2625c1814f934f2bb12
BugBuster99 <arvindj19962014@gmail.com>
2017-02-06 19:33:17 +0530

    Update stringsort.c

--- stringsort.c
+++ stringsort.c
@@ -5,7 +5,7 @@ int strcompare(char *x,char *y)
     int i;
     for(i=0;x[i]!='\0';i++)
     {
-        if(strcmp(x[i],y[i])>0)
+        if(strcmp(x[i],y[i])>=1)
         {
             return 1;
         }
